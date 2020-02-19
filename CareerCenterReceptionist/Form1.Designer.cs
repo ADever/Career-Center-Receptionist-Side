0a7f917accc0cd59f740974b6e60f110c2d37199
ADever <55519742+ADever@users.noreply.github.com>
2020-02-18 20:35:13 -0500

    Initial Commit

---
+++ CareerCenterReceptionist/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace CareerCenterReceptionist
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dateInput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(244, 138);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(362, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "New excel spreadsheet with today\'s data";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(244, 95);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(219, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "New daily table";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // dateInput
+            // 
+            this.dateInput.Location = new System.Drawing.Point(459, 289);
+            this.dateInput.Name = "dateInput";
+            this.dateInput.Size = new System.Drawing.Size(100, 22);
+            this.dateInput.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dateInput);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox dateInput;
+    }
+}
+
---
+++ CareerCenterReceptionist/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace CareerCenterReceptionist
+{
+    public partial class Form1 : Form
+    {
+        private SqlConnection connection;
+        private SqlCommand command;
+        private SqlConnection connection2;
+        private SqlCommand command2;
+        private SqlConnection connection3;
+        private SqlConnection connection4;
+        private SqlCommand command3;
+        private SqlCommand command4;
+
+
+        private string myDate = DateTime.Now.ToShortDateString();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            connection = new SqlConnection(" Data Source=localhost; initial Catalog = usrinfo; Integrated Security = true; pooling = false");
+            connection.Open();
+            command = new SqlCommand("INSERT INTO dailyDump(name, socialNum, date) SELECT name, socialNum, Date FROM usrinfo WHERE date = @a1", connection);
+            command.Parameters.AddWithValue("a1", myDate);
+            command.ExecuteNonQuery();
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                connection2 = new SqlConnection(" Data Source=localhost; initial Catalog = usrinfo; Integrated Security = true; pooling = false");
+                connection2.Open();
+                command2 = new SqlCommand("INSERT INTO specificDaily(name, socialNum, date) SELECT name, socialNum, Date FROM usrinfo WHERE date = @a1", connection2);
+                command2.Parameters.AddWithValue("a1", dateInput.Text);
+                command2.ExecuteNonQuery();
+            } finally
+            {
+                MessageBox.Show("This failed moron");
+            }
+        }
+    }
+}
